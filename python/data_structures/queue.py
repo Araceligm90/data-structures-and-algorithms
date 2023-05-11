1077c901d96551e3038be3dd3e48c33789d74216
Araceligm90 <garcia.araceli90@gmail.com>
2023-05-11 13:54:17 +0200

    queue passing

--- python/data_structures/queue.py
+++ python/data_structures/queue.py
@@ -1,10 +1,5 @@
 from data_structures.invalid_operation_error import InvalidOperationError
-
-
-class Node:
-    def __int__(self, value, next=None):
-        self.value = value
-        self.next = next
+from data_structures.linked_list import Node
 
 
 class Queue:
