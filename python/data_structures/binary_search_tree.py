a8e25c1c73e643504daf0b38a9f6f4efec73b0a0
Araceligm90 <garcia.araceli90@gmail.com>
2023-05-16 19:58:03 +0200

    tests for BT and BST passing

--- python/data_structures/binary_search_tree.py
+++ python/data_structures/binary_search_tree.py
@@ -1,4 +1,4 @@
-from data_structures.binary_tree import BinaryTree
+from data_structures.binary_tree import BinaryTree, Node
 
 
 class BinarySearchTree(BinaryTree):
@@ -6,10 +6,43 @@ class BinarySearchTree(BinaryTree):
     Put docstring here
     """
 
-    def __init__(self):
-        # initialization here
-        pass
+    def __init__(self, root=None):
+        self.root = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def add(self, value):
+        if not self.root:
+            self.root = Node(value)
+            return
+
+        current = self.root
+
+        while current:
+            if value < current.value:
+                if current.left is None:
+                    current.left = Node(value)
+                    return
+                current = current.left
+            else:
+                if current.right is None:
+                    current.right = Node(value)
+                    return
+                current = current.right
+
+    def contains(self, value):
+        if self.root is None:
+            return False
+
+        current = self.root
+
+        while current is not None:
+            if current.value == value:
+                return True
+            if current.value > value:
+                current = current.left
+            else:
+                current = current.right
+
+        return False
+
+
+
--- python/data_structures/binary_tree.py
+++ python/data_structures/binary_tree.py
@@ -7,16 +7,41 @@ class BinaryTree:
         self.root = root
 
     def pre_order(self):
-        tree: []
-        current = self.root
+        def traverse(node, result):
+            # If node id not None, append its value to the result list and recursively call traverse on its left and
+            # right child nodes
+            if node is not None:
+                result.append(node.value)
+                traverse(node.left, result)
+                traverse(node.right, result)
+
+        # Initialize an empty list to accumulate the values
+        result = []
+        # Call the traverse function on the root node to perform the traversal
+        traverse(self.root, result)
+        return result
 
     def post_order(self):
-        tree: []
-        current = self.root
+        def traverse(node, result):
+            if node is not None:
+                traverse(node.left, result)
+                traverse(node.right, result)
+                result.append(node.value)
+
+        result = []
+        traverse(self.root, result)
+        return result
 
     def in_order(self):
-        tree: []
-        current = self.root
+        def traverse(node, result):
+            if node is not None:
+                traverse(node.left, result)
+                result.append(node.value)
+                traverse(node.right, result)
+
+        result = []
+        traverse(self.root, result)
+        return result
 
 
 class Node:
